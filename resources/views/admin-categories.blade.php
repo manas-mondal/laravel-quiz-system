e0320dbd60845488ce3eb2c7f3202f49b42fb0dc
manas-mondal <manasmondal035@gmail.com>
2025-09-11 07:11:22 +0000

    adding eye icon for view quiz

--- resources/views/admin-categories.blade.php
+++ resources/views/admin-categories.blade.php
@@ -82,7 +82,8 @@
                         <td class="px-4 py-2 border-b border-blue-100 text-gray-600">{{ $category->id }}</td>
                         <td class="px-4 py-2 border-b border-blue-100 text-gray-600">{{ $category->name }}</td>
                         <td class="px-4 py-2 border-b border-blue-100 text-gray-600">{{ $category->creator }}</td>
-                        <td class="px-4 py-2 border-b border-blue-100">
+                        <td class="px-4 py-2 border-b border-blue-100 flex space-x-2">
+                            <!-- Delete Button -->
                             <form action="{{ route('admin.category.delete', ['id' => $category->id]) }}" method="POST"
                                 onsubmit="return confirm('Are you sure you want to delete this category?');">
                                 @csrf
@@ -98,6 +99,17 @@
                                     </svg>
                                 </button>
                             </form>
+                            <!-- View Button -->
+                            <a href="" class="text-gray-700 hover:text-blue-500 transition-colors"
+                                title="View">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                            </a>
                         </td>
                     </tr>
                 @endforeach
