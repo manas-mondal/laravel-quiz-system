e4a31aa2885fb105bb16e42bc8eafd912ef9d180
manas-mondal <manasmondal035@gmail.com>
2025-10-08 16:19:27 +0000

    create records migration with Record model

---
+++ app/Models/Record.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Record extends Model
+{
+    protected $fillable = [
+        'quiz_id',
+        'user_id',
+        'status',
+    ];
+
+    public function quiz()
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
