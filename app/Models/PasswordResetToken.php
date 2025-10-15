39a93bf32e04ac4dc3a7813c6e193565ceb6de04
manas-mondal <manasmondal035@gmail.com>
2025-10-15 14:10:46 +0000

    update PasswordResetToken model

--- app/Models/PasswordResetToken.php
+++ app/Models/PasswordResetToken.php
@@ -7,9 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class PasswordResetToken extends Model
 {
-    use HasFactory;
+   use HasFactory;
 
     protected $table = 'password_reset_tokens';
-    protected $fillable = ['email', 'token', 'created_at'];
-    public $timestamps = false;
+
+    // Primary key is 'id'
+    protected $primaryKey = 'id';
+
+    public $timestamps = false; // created_at manually set, no updated_at
+
+    protected $fillable = [
+        'email',
+        'token',
+        'created_at',
+    ];
 }
